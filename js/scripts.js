4c4536dbb94cad2d7ddd1c0c12a95c2d17f4be74
Claudio-Pri <priv.claudio@gmail.com>
2024-07-25 16:39:28 +0200

    js- timer- messaggio tempo scaduto

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ const min = 1;
 const max = 10;
 const randomNumbers = [];
 const numbersContainer = document.getElementById('numbers-container');
+const timeToWait = 2;
+// generazione numeri casuali
 for (let i = 0; i < 5; i++) {
     const num = getRndNumber(min, max);
     console.log(num);
@@ -17,6 +19,16 @@ for (let i = 0; i < 5; i++) {
     </div>
     `
 }
+// impostazione timer
+setTimeout(function () {
+    console.log('tempo scaduto');
+    numbersContainer.innerHTML = `
+    <h1>
+        Inserisci i numeri nell' ordine corretto
+    </h1>
+    `
+}, timeToWait * 1000);
+
 
 
 //===== Functions =====
