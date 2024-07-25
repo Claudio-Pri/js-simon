00c2a5d2ad7fab47d2dee9c5e3293d10e83db608
Claudio-Pri <priv.claudio@gmail.com>
2024-07-25 16:28:49 +0200

    js- generazione numeri e stampa in pagina

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
-console.log('js collegato');
-
 //===== Appunti ======
 // ` backtick
 
 //===== Code ======
+const min = 1;
+const max = 10;
+const randomNumbers = [];
+const numbersContainer = document.getElementById('numbers-container');
+for (let i = 0; i < 5; i++) {
+    const num = getRndNumber(min, max);
+    console.log(num);
+    randomNumbers.push(num);
+    console.log(randomNumbers);
+    numbersContainer.innerHTML += `
+    <div>
+        ${num}
+    </div>
+    `
+}
 
 
 //===== Functions =====
+function getRndNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
